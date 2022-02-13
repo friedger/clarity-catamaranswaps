d3c58fae7795584002d275f8ff4e443495f7a45b
Friedger Müffke <mail@friedger.de>
2022-02-13 20:48:19 +0100

    add atomic-satoshibles

--- src/deps.ts
+++ src/deps.ts
@@ -1,19 +1,22 @@
 export type {
   Account,
   ReadOnlyFn,
-} from "https://deno.land/x/clarinet@v0.24.0/index.ts";
+} from "https://deno.land/x/clarinet@v0.26.0/index.ts";
 
 export {
   Clarinet,
   Chain,
   Tx,
   types,
-} from "https://deno.land/x/clarinet@v0.24.0/index.ts";
+} from "https://deno.land/x/clarinet@v0.26.0/index.ts";
 
-export { assertEquals, assertObjectMatch } from "https://deno.land/std@0.106.0/testing/asserts.ts";
+export {
+  assertEquals,
+  assertObjectMatch,
+} from "https://deno.land/std@0.125.0/testing/asserts.ts";
 
 export {
   beforeEach,
   describe,
   it,
-} from "https://deno.land/x/test_suite@v0.8.0/mod.ts";
+} from "https://deno.land/x/test_suite@0.9.5/mod.ts";
